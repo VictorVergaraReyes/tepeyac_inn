273f13423132464e7605e7fb53803d107918dfd0
Victor Vergara Reyes <vvergara@ahorra.io>
2023-06-12 09:16:02 -0600

    página principal

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,14 @@ export default function IndexPage(){
       <h1>Index Page</h1>
       <div>
         <h2>Acerca de nosotros</h2>
-        <div className="container">
+        <div className="col-12">Imagen grande con slogan</div>
+        <div className="col-12">
+          <p>
+            <h2>Descripcion de el salón</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, illum deleniti! Qui officia sapiente atque ratione odit reprehenderit asperiores adipisci amet, dolorem iusto quis molestiae necessitatibus. Quia assumenda velit nam tenetur omnis ad hic rem incidunt provident delectus nobis laborum odio quos beatae facere, veniam repellat, minima iure odit amet!</p>
+          </p>
+        </div>
+        <div className="row">
           <p className="col-12 col-md-6 col-lg-6">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam earum, alias ex vitae quibusdam nulla, sit excepturi voluptate molestiae similique enim fuga dolorum laborum nisi! Harum, odit excepturi atque mollitia quod eos sed aut magni, laborum asperiores fuga eligendi illo hic, adipisci unde. Suscipit neque nemo repellendus sint cumque minus!
           </p> 

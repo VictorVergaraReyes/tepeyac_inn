0e81917afc0693f4d574d18a93434770493ba1ce
Victor Vergara Reyes <vvergara@ahorra.io>
2023-06-14 22:20:58 -0600

    paquete

--- app/paquetes/page.jsx
+++ app/paquetes/page.jsx
@@ -5,6 +5,23 @@ function Paquetes(){
     <div>
       <h1>Paquetes</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae deleniti sed ducimus, atque minus quaerat ex beatae inventore maiores cupiditate amet quod, dolores illum excepturi suscipit quisquam, iure rerum corrupti. Corporis porro saepe sequi. Sapiente rerum atque incidunt provident nostrum voluptatem ratione doloremque blanditiis aliquid eum libero, ex, debitis praesentium eaque alias voluptatum sit sed, facilis cum nesciunt odit perspiciatis est dicta perferendis? Veritatis vitae ad similique molestias laborum, aspernatur quo officia deleniti pariatur nisi nulla tempore, voluptatibus distinctio non sed maiores deserunt. In reiciendis amet maxime sapiente quod expedita tempore, a itaque modi repudiandae officiis laudantium omnis eius ad.</p>
+      <div className="row">
+        <h1>Paquetes de boda</h1>
+        <div>
+          <div className="col-12 col-md-12 col-lg-4">
+            <h2>Paquete 1</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid modi unde tenetur praesentium. Laboriosam, consequuntur dolores? Molestias provident nesciunt quod voluptatibus veniam porro sit, culpa vel tenetur est accusamus inventore ipsum dicta, aliquam deserunt eos consequatur. Sapiente, aliquam laudantium. Quasi.</p>
+          </div>
+          <div className="col-12 col-md-12 col-lg-4">
+          <h2>Paquete 2</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid modi unde tenetur praesentium. Laboriosam, consequuntur dolores? Molestias provident nesciunt quod voluptatibus veniam porro sit, culpa vel tenetur est accusamus inventore ipsum dicta, aliquam deserunt eos consequatur. Sapiente, aliquam laudantium. Quasi.</p>
+          </div>
+          <div className="col-12 col-md-12 col-lg-4">
+          <h2>Paquete 1</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid modi unde tenetur praesentium. Laboriosam, consequuntur dolores? Molestias provident nesciunt quod voluptatibus veniam porro sit, culpa vel tenetur est accusamus inventore ipsum dicta, aliquam deserunt eos consequatur. Sapiente, aliquam laudantium. Quasi.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

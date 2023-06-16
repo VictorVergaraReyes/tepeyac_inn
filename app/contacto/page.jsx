5ac72251a8d7bd12a5cab42ee5671a5ef6e2e390
Victor Vergara Reyes <vvergara@ahorra.io>
2023-06-16 10:05:12 -0600

    paquetes y contacto

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -1,8 +1,15 @@
 export default function Contacto(){
     return(
-      <div>
-        <h1>Contacto</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae deleniti sed ducimus, atque minus quaerat ex beatae inventore maiores cupiditate amet quod, dolores illum excepturi suscipit quisquam, iure rerum corrupti. Corporis porro saepe sequi. Sapiente rerum atque incidunt provident nostrum voluptatem ratione doloremque blanditiis aliquid eum libero, ex, debitis praesentium eaque alias voluptatum sit sed, facilis cum nesciunt odit perspiciatis est dicta perferendis? Veritatis vitae ad similique molestias laborum, aspernatur quo officia deleniti pariatur nisi nulla tempore, voluptatibus distinctio non sed maiores deserunt. In reiciendis amet maxime sapiente quod expedita tempore, a itaque modi repudiandae officiis laudantium omnis eius ad.</p>
-      </div>
+      <section>
+        <div className="text-center">Imagen</div>
+        <h1 className="text-center">Contacto</h1>
+        <div className="container">
+          <ul >
+            <p>Dirección: Calle unión  numero 216, Mexico City, Mexico, 07020</p>
+            <p>Teléfono: 55 1289 0770</p>
+            <p>Correo: salontepeyacinn.info@gmail.com</p>
+          </ul>
+        </div>
+      </section>
     )
   }

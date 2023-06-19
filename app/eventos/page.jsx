fc9f43d19def8ae50127f7bb60b654d5bb8cfce0
Victor Vergara Reyes <vvergara@ahorra.io>
2023-06-19 17:49:34 -0600

    eventos

--- app/eventos/page.jsx
+++ app/eventos/page.jsx
@@ -1,9 +1,47 @@
 
 export default function EventosPage(){
   return(
-    <div>
+    <div className="p-3">
       <h1>Eventos</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae deleniti sed ducimus, atque minus quaerat ex beatae inventore maiores cupiditate amet quod, dolores illum excepturi suscipit quisquam, iure rerum corrupti. Corporis porro saepe sequi. Sapiente rerum atque incidunt provident nostrum voluptatem ratione doloremque blanditiis aliquid eum libero, ex, debitis praesentium eaque alias voluptatum sit sed, facilis cum nesciunt odit perspiciatis est dicta perferendis? Veritatis vitae ad similique molestias laborum, aspernatur quo officia deleniti pariatur nisi nulla tempore, voluptatibus distinctio non sed maiores deserunt. In reiciendis amet maxime sapiente quod expedita tempore, a itaque modi repudiandae officiis laudantium omnis eius ad.</p>
+      <main>
+        <section className="row">
+          <div className="col-12 col-md-6 col-md-6">
+            <h2>XV años</h2>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti laboriosam sunt necessitatibus similique quia error quaerat libero iusto perspiciatis ea! Voluptatibus eligendi facilis molestias non et culpa voluptate quidem. Tenetur quod aspernatur animi, sint eveniet aperiam provident repellat qui et fuga facilis? Exercitationem aperiam cum magnam ipsum, eligendi non quas!
+          </div>
+          <div className="col-12 col-md-6 col-md-6">
+            Aqui va una imagen
+          </div>
+        </section>
+        <section className="row">
+          <div className="col-12 col-md-6 col-md-6">
+            Aqui va una imagen
+          </div>
+          <div className="col-12 col-md-6 col-md-6">
+          <h2>Bodas</h2>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti laboriosam sunt necessitatibus similique quia error quaerat libero iusto perspiciatis ea! Voluptatibus eligendi facilis molestias non et culpa voluptate quidem. Tenetur quod aspernatur animi, sint eveniet aperiam provident repellat qui et fuga facilis? Exercitationem aperiam cum magnam ipsum, eligendi non quas!
+          </div>
+        </section>
+        <section className="row">
+          <h2></h2>
+          <div className="col-12 col-md-6 col-md-6">
+          <h2>Graduaciones</h2>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti laboriosam sunt necessitatibus similique quia error quaerat libero iusto perspiciatis ea! Voluptatibus eligendi facilis molestias non et culpa voluptate quidem. Tenetur quod aspernatur animi, sint eveniet aperiam provident repellat qui et fuga facilis? Exercitationem aperiam cum magnam ipsum, eligendi non quas!
+          </div>
+          <div className="col-12 col-md-6 col-md-6">
+            Aqui va una imagen
+          </div>
+        </section>
+        <section className="row">
+          <div className="col-12 col-md-6 col-md-6">
+            Aqui va una imagen
+          </div>
+          <div className="col-12 col-md-6 col-md-6">
+          <h2>Comuniones, bautizos</h2>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti laboriosam sunt necessitatibus similique quia error quaerat libero iusto perspiciatis ea! Voluptatibus eligendi facilis molestias non et culpa voluptate quidem. Tenetur quod aspernatur animi, sint eveniet aperiam provident repellat qui et fuga facilis? Exercitationem aperiam cum magnam ipsum, eligendi non quas!
+          </div>
+        </section>
+      </main>
     </div>
   )
 }

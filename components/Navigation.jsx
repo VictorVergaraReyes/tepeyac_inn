1e2e07b7b01d9e4fa06735e9c5c9616a63bb2384
Victor Vergara Reyes <vvergara@ahorra.io>
2023-06-20 13:50:32 -0600

    menu hamburguesa

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,10 +1,16 @@
+'use client'
 import Link from "next/link"
 import 'bootstrap/dist/css/bootstrap.css';
 import Image from "next/image";
+import { useState } from "react";
+
 
 export default function Navigation(){
-    return(    
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    const [menu,setMenu]=useState(false)
+    const toogleMenu=()=>{setMenu(!menu)}
+    return(
+    <section>
+        <nav className="navbar navbar-expand-lg navbar-expand-md bg-body-tertiary">
         <div className="container-fluid">
             <div>
                 <Image id="logoNav"
@@ -14,7 +20,7 @@ export default function Navigation(){
                 height={50}
                 />
             </div>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={toogleMenu}>
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
@@ -38,5 +44,13 @@ export default function Navigation(){
             </div>
         </div>
         </nav>
+        <div className={`columns ${menu?'d-flex':'d-none'}`}>
+        <ul><Link className="nav-link active" aria-current="page" href="/">Home</Link></ul>
+        <ul><Link className="nav-link" href="/eventos">Eventos</Link></ul>
+        <ul><Link className="nav-link" href="/galeria">Galeria</Link></ul>
+        <ul><Link className="nav-link" href="/paquetes">Paquetes</Link></ul>
+        <ul><Link className="nav-link" href="/contacto">Contacto</Link></ul>
+    </div>
+    </section>  
     )
 }

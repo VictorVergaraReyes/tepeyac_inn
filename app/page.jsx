e79381b03bd04cd515bfde2dfde9790f9f8e65d0
Victor Vergara Reyes <vvergara@ahorra.io>
2023-06-23 16:10:12 -0600

    cambios de figma background color

--- app/page.jsx
+++ app/page.jsx
@@ -1,49 +1,33 @@
 import React from "react";
+import styles from "./page.module.css"
 
 export default function IndexPage(){
   return(
-    <main className="bg-dark-subtle">
+    <main  className={styles.main}>
       <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
+      {/* Banner */}
+        <section>
         <div style={{
-           backgroundImage: `url(${"/header_salon.jpg"})`,
+           backgroundImage: `url(${"/imagenes/banner.jpg"})`,
            backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
-           height: '100vh'
-           }}>
-            <div className="position-absolute top-50 start-50 translate-middle">
-              <p className="text-white fs-1">Contenido del banner </p>
-            </div>
-          
+           height: '100vh',
+           opacity:0.3
+           }}>            
         </div>
-        <section className="container">
-        <h1 className="text-center">Quienes somos</h1>
-        <div className="col-12">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, illum deleniti! Qui officia sapiente atque ratione odit reprehenderit asperiores adipisci amet, dolorem iusto quis molestiae necessitatibus. Quia assumenda velit nam tenetur omnis ad hic rem incidunt provident delectus nobis laborum odio quos beatae facere, veniam repellat, minima iure odit amet!</p>
-        </div>
-        <div className="row">
-          <p className="col-12 col-md-6 col-lg-6">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam earum, alias ex vitae quibusdam nulla, sit excepturi voluptate molestiae similique enim fuga dolorum laborum nisi! Harum, odit excepturi atque mollitia quod eos sed aut magni, laborum asperiores fuga eligendi illo hic, adipisci unde. Suscipit neque nemo repellendus sint cumque minus!
-          </p> 
-          <div className="col-12 col-md-6 col-lg-6">
-            Imagen
+          <div className="position-absolute top-50 start-50 translate-middle">
+            <p className="text-white fs-1 text-center">Contenido del banner </p>
+          </div> 
+        </section>
+            
+        <section className="row d-flex m-4">
+          <h1 className="text-center">Acerca de nosotros</h1>
+          <div className="">
+            <p className="slogan">
+            En Salón Tepeyac inn  tenemos amplia experiencia en nuestro servicio confía en nosotros no te defraudaremos 
+Cada evento que organizamos es como si fuera nuestro!
+            </p>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-12 col-md-6 col-lg-6">
-            Imagen
-          </div>
-          <p className="col-12 col-md-6 col-lg-6">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam earum, alias ex vitae quibusdam nulla, sit excepturi voluptate molestiae similique enim fuga dolorum laborum nisi! Harum, odit excepturi atque mollitia quod eos sed aut magni, laborum asperiores fuga eligendi illo hic, adipisci unde. Suscipit neque nemo repellendus sint cumque minus!
-          </p> 
-        </div>
-        <div className="row">
-          <p className="col-12 col-md-6 col-lg-6">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam earum, alias ex vitae quibusdam nulla, sit excepturi voluptate molestiae similique enim fuga dolorum laborum nisi! Harum, odit excepturi atque mollitia quod eos sed aut magni, laborum asperiores fuga eligendi illo hic, adipisci unde. Suscipit neque nemo repellendus sint cumque minus!
-          </p> 
-          <div className="col-12 col-md-6 col-lg-6">
-            Imagen
-          </div>
-        </div>
         </section>
     </main>
   )

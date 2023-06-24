cdfd889c2fd58be9ed74ec0ba59260d7ab514ce8
Victor Vergara Reyes <vvergara@ahorra.io>
2023-06-23 18:32:33 -0600

    margenes pagina principal

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./page.module.css"
+import Navigation from "@/components/Navigation";
 
 export default function IndexPage(){
   return(
-    <main  className={styles.main}>
+    <body  className={styles.main}>
+      <Navigation></Navigation>
       <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
       {/* Banner */}
-        <section>
+        <section className={styles.banner}>
         <div style={{
            backgroundImage: `url(${"/imagenes/banner.jpg"})`,
            backgroundRepeat: 'no-repeat',
@@ -20,15 +22,15 @@ export default function IndexPage(){
           </div> 
         </section>
             
-        <section className="row d-flex m-4">
+        <section className={styles.acerca}>
           <h1 className="text-center">Acerca de nosotros</h1>
-          <div className="">
-            <p className="slogan">
+          <div className="row align-items-center">
+            <p className={styles.slogan}>
             En Salón Tepeyac inn  tenemos amplia experiencia en nuestro servicio confía en nosotros no te defraudaremos 
 Cada evento que organizamos es como si fuera nuestro!
             </p>
           </div>
         </section>
-    </main>
+    </body>
   )
 }

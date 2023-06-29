15a52eb8f2fd25b7cfa15e22b16e595e0150f475
vicvergara <vvergara@ahorra.io>
2023-06-29 09:38:36 -0600

    imagenes eventos

--- app/eventos/page.jsx
+++ app/eventos/page.jsx
@@ -1,5 +1,6 @@
 import Navigation from "@/components/Navigation"
 import styles from "./page.module.css"
+import Image from "next/image"
 
 export default function EventosPage(){
   return(
@@ -13,12 +14,12 @@ export default function EventosPage(){
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti laboriosam sunt necessitatibus similique quia error quaerat libero iusto perspiciatis ea! Voluptatibus eligendi facilis molestias non et culpa voluptate quidem. Tenetur quod aspernatur animi, sint eveniet aperiam provident repellat qui et fuga facilis? Exercitationem aperiam cum magnam ipsum, eligendi non quas!
           </div>
           <div className="col-12 col-md-6 col-md-6">
-            Aqui va una imagen
+            <Image src={"/imagenes/XV.svg"} width={300} height={200} alt="quince años"/>
           </div>
         </section>
         <section className="row">
           <div className="col-12 col-md-6 col-md-6">
-            Aqui va una imagen
+            <Image src={"/imagenes/wedding.svg"} width={300} height={200} alt="boda"/>
           </div>
           <div className="col-12 col-md-6 col-md-6">
           <h2>Bodas</h2>
@@ -32,12 +33,12 @@ export default function EventosPage(){
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti laboriosam sunt necessitatibus similique quia error quaerat libero iusto perspiciatis ea! Voluptatibus eligendi facilis molestias non et culpa voluptate quidem. Tenetur quod aspernatur animi, sint eveniet aperiam provident repellat qui et fuga facilis? Exercitationem aperiam cum magnam ipsum, eligendi non quas!
           </div>
           <div className="col-12 col-md-6 col-md-6">
-            Aqui va una imagen
+          <Image src={"/imagenes/graduacion2.svg"} width={300} height={200} alt="graduacion"/>
           </div>
         </section>
         <section className="row">
           <div className="col-12 col-md-6 col-md-6">
-            Aqui va una imagen
+          <Image src={"/imagenes/bautizo.svg"} width={300} height={200} alt="bautizo"/>
           </div>
           <div className="col-12 col-md-6 col-md-6">
           <h2>Comuniones, bautizos</h2>

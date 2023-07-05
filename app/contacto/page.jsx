0554898d9bac758690522bfe6567d5ac82c206a7
vicvergara <vvergara@ahorra.io>
2023-07-05 17:20:06 -0600

    eliminando espacios en blanco

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -4,74 +4,75 @@ export default function Contacto(){
   return(
       <main className={styles.main}>
         <Navigation></Navigation>
-        <div className="container-fluid row align-items-start">
-        <h1 className="text-center">Contacto</h1>
-        
-        {/* Formulario */}
-        <form className="col-12 col-lg-6">
-        <div className="mb-3">
-            <label for="exampleFormControlInput1" className="form-label">Nombre</label>
-            <input type="email" className="form-control" id="exampleFormControlInput1"/>
-          </div>
+        <div className="container-fluid row align-items-start mt-5">
+          <h1 className="text-center">Contacto</h1>
+          
+          {/* Formulario */}
+          <form className="col-12 col-lg-6">
           <div className="mb-3">
-            <label for="exampleFormControlInput1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-          </div>
-          <div className="mb-3">
-            <label for="exampleFormControlInput1" className="form-label">Numero de teléfono</label>
-            <input type="email" className="form-control" id="exampleFormControlInput1"/>
-          </div>
-          <label for="seleccionEvento" className="form-label">Selecciona el tipo de evento</label>
-          <select id="seleccionEvento" class="form-select mb-3" aria-label="Default select example">
-            <option selected>Selecciona el tipo de evento</option>
-            <option value="1">XV años</option>
-            <option value="2">Boda</option>
-            <option value="3">Bautizo</option>
-            <option value="4">Salidas escolares</option>
-            <option value="4">Otro</option>
-          </select>
-          {/* Fecha */}
-          <section className="row">
-            <div className="col-12 col-md-6 col-lg-6">
-              <label for="date" className="form-label">Fecha:</label>
-              <input type="date" className="form-control" id="date" required/>
-            </div>
-            {/* No de personas */}
-            <div className="col-12 col-md-6 col-lg-6">
-              <label for="exampleFormControlInput1" className="form-label">Número de personas</label>
+              <label for="exampleFormControlInput1" className="form-label">Nombre</label>
               <input type="email" className="form-control" id="exampleFormControlInput1"/>
             </div>
-          </section>
-          
-          <div className="mb-3">
-            <label for="exampleFormControlTextarea1" className="form-label">Describenos el evento</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
-        </form>
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" className="form-label">Numero de teléfono</label>
+              <input type="email" className="form-control" id="exampleFormControlInput1"/>
+            </div>
+            <label for="seleccionEvento" className="form-label">Selecciona el tipo de evento</label>
+            <select id="seleccionEvento" class="form-select mb-3" aria-label="Default select example">
+              <option selected>Selecciona el tipo de evento</option>
+              <option value="1">XV años</option>
+              <option value="2">Boda</option>
+              <option value="3">Bautizo</option>
+              <option value="4">Salidas escolares</option>
+              <option value="4">Otro</option>
+            </select>
+            {/* Fecha */}
+            <section className="row">
+              <div className="col-12 col-md-6 col-lg-6">
+                <label for="date" className="form-label">Fecha:</label>
+                <input type="date" className="form-control" id="date" required/>
+              </div>
+              {/* No de personas */}
+              <div className="col-12 col-md-6 col-lg-6">
+                <label for="exampleFormControlInput1" className="form-label">Número de personas</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1"/>
+              </div>
+            </section>
+            
+            <div className="mb-3">
+              <label for="exampleFormControlTextarea1" className="form-label">Describenos el evento</label>
+              <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+          </form>
 
-        {/* Informacion de contacto */}
-        <div className="col-12 col-lg-6">
-          <ul >
-            <p>Dirección: Calle unión  numero 216, Mexico City, Mexico, 07020</p>
-            <p>Teléfono: 55 1289 0770</p>
-            <p>Correo: salontepeyacinn.info@gmail.com</p>
-            <p>Horario</p>
-          </ul>
+          {/* Informacion de contacto */}
+          <div className="col-12 col-lg-6">
+            <ul >
+              <p>Dirección: Calle unión  numero 216, Mexico City, Mexico, 07020</p>
+              <p>Teléfono: 55 1289 0770</p>
+              <p>Correo: salontepeyacinn.info@gmail.com</p>
+              <p>Horario</p>
+            </ul>
 
-          {/*Mapa */}
-          <section className="d-flex justify-content-center">
-            <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.3499990006663!2d-99.12313612834393!3d19.481419364010307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f99fe6c9d559%3A0x10765da04acbcb7b!2sSal%C3%B3n%20de%20Fiestas%20Tepeyac%20Inn!5e0!3m2!1ses-419!2smx!4v1686669005646!5m2!1ses-419!2smx" 
-            width="600" 
-            height="450" 
-            allowfullscreen="" 
-            loading="lazy" 
-            style={{border:"0"}}
-            referrerpolicy="no-referrer-when-downgrade">
-            </iframe>
-          </section>
-        </div >
+            {/*Mapa */}
+            <section className="d-flex justify-content-center">
+              <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.3499990006663!2d-99.12313612834393!3d19.481419364010307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f99fe6c9d559%3A0x10765da04acbcb7b!2sSal%C3%B3n%20de%20Fiestas%20Tepeyac%20Inn!5e0!3m2!1ses-419!2smx!4v1686669005646!5m2!1ses-419!2smx" 
+              width="600" 
+              height="450" 
+              allowfullscreen="" 
+              loading="lazy" 
+              style={{border:"0"}}
+              referrerpolicy="no-referrer-when-downgrade">
+              </iframe>
+            </section>
+          </div >
         </div>
+        {/* Redes sociales */}
       </main>
     )
   }
--- app/paquetes/page.jsx
+++ app/paquetes/page.jsx
@@ -1,13 +1,11 @@
-import React from "react";
 import Image from "next/image";
 import Navigation from "@/components/Navigation";
 import styles from "./page.module.css";
 function Paquetes(){
   return(
-    <main className={styles.main}>
+    <div className={styles.main}>
       <Navigation></Navigation>
-      <h1 className="text-center">Paquetes</h1>
-      <div className="container">
+      <div className="container mt-4 mb-4">
         <h1>Paquetes de boda</h1>
         <div className="row align-items-start">
         <div className="col-12 col-md-12 col-lg-4" styles={`{width: 18rem}`}>
@@ -15,58 +13,79 @@ function Paquetes(){
           <div className="card-body">
             <h5 className="card-title">Paquete 1</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
           <div className="col-12 col-md-12 col-lg-4" styles={`{width: 18rem}`}>
           <Image src="/logo.jpg" className="card-img-top" width={200} height={150} alt="paquete 1"/>
           <div className="card-body">
-            <h5 className="card-title">Paquete 1</h5>
+            <h5 className="card-title">Paquete 2</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
         <div className="col-12 col-md-12 col-lg-4" styles={`{width: 18rem}`}>
           <Image src="/logo.jpg" className="card-img-top" width={200} height={150} alt="paquete 1"/>
           <div className="card-body">
-            <h5 className="card-title">Paquete 1</h5>
+            <h5 className="card-title">Paquete 3</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
         </div>
       </div>
 
-      <div className="container">
-        <h1>Paquetes de boda</h1>
+      <div className="container mt-4 mb-4">
+        <h1>Paquetes de XV años</h1>
         <div className="row align-items-start">
         <div className="col-12 col-md-12 col-lg-4" styles={`{width: 18rem}`}>
           <Image src="/logo.jpg" className="card-img-top" width={200} height={150} alt="paquete 1"/>
           <div className="card-body">
             <h5 className="card-title">Paquete 1</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
           <div className="col-12 col-md-12 col-lg-4" styles={`{width: 18rem}`}>
           <Image src="/logo.jpg" className="card-img-top" width={200} height={150} alt="paquete 1"/>
           <div className="card-body">
-            <h5 className="card-title">Paquete 1</h5>
+            <h5 className="card-title">Paquete 2</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
         <div className="col-12 col-md-12 col-lg-4" styles={`{width: 18rem}`}>
           <Image src="/logo.jpg" className="card-img-top" width={200} height={150} alt="paquete 1"/>
           <div className="card-body">
-            <h5 className="card-title">Paquete 1</h5>
+            <h5 className="card-title">Paquete 3</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
         </div>
       </div>
-    </main>
+
+      <div className="container mt-4">
+        <h1>Paquetes graduaciones</h1>
+        <div className="row align-items-start">
+        <div className="col-12 col-md-12 col-lg-4" styles={`{width: 18rem}`}>
+          <Image src="/logo.jpg" className="card-img-top" width={200} height={150} alt="paquete 1"/>
+          <div className="card-body">
+            <h5 className="card-title">Paquete 1</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+          </div>
+        </div>
+          <div className="col-12 col-md-12 col-lg-4" styles={`{width: 18rem}`}>
+          <Image src="/logo.jpg" className="card-img-top" width={200} height={150} alt="paquete 1"/>
+          <div className="card-body">
+            <h5 className="card-title">Paquete 2</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-12 col-lg-4" styles={`{width: 18rem}`}>
+          <Image src="/logo.jpg" className="card-img-top" width={200} height={150} alt="paquete 1"/>
+          <div className="card-body">
+            <h5 className="card-title">Paquete 3</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
